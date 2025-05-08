2ddb0d6c5334e6ce79b27c0e5a5dba26fedb1a59
Wolf McNally <wolf@wolfmcnally.com>
2025-05-07 17:08:15 -0700

    Initial deploy of zewif documentation

---
+++ orchard/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["MerkleHashOrchard","OrchardSentOutput","OrchardWitness"]};
---
+++ sapling/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["Address","MerkleHashSapling","SaplingExtendedFullViewingKey","SaplingExtendedSpendingKey","SaplingIncomingViewingKey","SaplingSentOutput","SaplingWitness"]};
